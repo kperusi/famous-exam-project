408869bb77798de8452b299f6ef3986754043c88
Sunday Famous <106285754+kperusi@users.noreply.github.com>
2022-11-08 10:22:17 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -37,7 +37,7 @@ export default function Home() {
           <button
             className="home-btn"
             onClick={() => {
-              navigate("/repository");
+              navigate("/repos");
             }}
           >
             Repository
